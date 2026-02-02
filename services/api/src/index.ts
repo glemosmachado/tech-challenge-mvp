0c8a3413c96cd0309c471209c80db3025cd9f152
Gabriel Machado <glemosmachado76@gmail.com>
2026-02-02 00:26:53 -0300

    jest testing

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,13 +1,7 @@
-import express from "express";
-
-const app = express();
-app.use(express.json());
-
-app.get("/health", (_, res) => {
-  res.json({ status: "ok" });
-});
+import { app } from "./app";
 
 const PORT = process.env.PORT || 3333;
+
 app.listen(PORT, () => {
   console.log(`API running on port ${PORT}`);
 });
